10127d2c264200c11e5edcc99bed50837e0f7a0a
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-08-01 09:04:23 +0530

    new commit

---
+++ Serialization&Deserialization.js
@@ -0,0 +1,20 @@
+var obj={id:101,name:'kshitiz'}
+undefined
+typeof obj
+'object'
+var json=JSON.stringify
+undefined
+json
+ƒ stringify() { [native code] }
+var json=JSON.stringify(obj)
+undefined
+json
+'{"id":101,"name":"kshitiz"}'
+obj
+{id: 101, name: 'kshitiz'}
+json.id
+undefined
+var t=JSON.parse(json)
+undefined
+t
+{id: 101, name: 'kshitiz'}
